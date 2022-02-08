2f8c118b8c6ccbb5b8605c05eb78379fd81dce23
Mehdi <mehdi.b.chikha@gmail.com>
2022-02-08 05:14:25 +0100

    refactor CarSerializer

--- SRP/src/com/directi/training/srp/solution_exercice/CarSerializer.java
+++ SRP/src/com/directi/training/srp/solution_exercice/CarSerializer.java
@@ -11,20 +11,13 @@ public class CarSerializer implements ICarSerializer {
     public String getCarsNames(List<Car> cars) {
         StringBuilder sb = new StringBuilder();
         for (Car car : cars) {
-            sb.append(car.getBrand());
-            sb.append(" ");
-            sb.append(car.getModel());
+            sb.append(this.getCarName(car));
             sb.append(", ");
         }
         return sb.substring(0, sb.length() - 2);
     }
 
     public String getCarName(Car car) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(car.getBrand());
-        sb.append(" ");
-        sb.append(car.getModel());
-        sb.append(", ");
-        return sb.substring(0, sb.length() - 2);
+        return car.getBrand() + car.getModel();
     }
 }
