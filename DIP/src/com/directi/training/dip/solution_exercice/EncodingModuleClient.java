d134c8d143f5a9893d0a1ddd3b3698ca5faaf0d7
Mehdi <mehdi.b.chikha@gmail.com>
2022-02-20 23:41:09 +0100

    refactored DIP Module

--- DIP/src/com/directi/training/dip/solution_exercice/EncodingModuleClient.java
+++ DIP/src/com/directi/training/dip/solution_exercice/EncodingModuleClient.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 
 public class EncodingModuleClient
 {
+    final static String BEFORE_ENCRYPTION_FILE = "DIP/src/com/directi/training/dip/solution_exercice/beforeEncryption.txt";
+    final static String AFTER_ENCRYPTION_FILE = "DIP/src/com/directi/training/dip/solution_exercice/afterEncryption.txt";
     public static void main(String[] args) throws IOException
     {
         EncodingModule encodingModule = new EncodingModule();
 
-        IReader reader = new MyFileReader(
-            "DIP/src/com/directi/training/dip/solution_exercice/beforeEncryption.txt");
-        IWriter writer = new MyFileWriter(
-            "DIP/src/com/directi/training/dip/solution_exercice/afterEncryption.txt");
+        IReader reader = new MyFileReader(BEFORE_ENCRYPTION_FILE);
+        IWriter writer = new MyFileWriter(AFTER_ENCRYPTION_FILE);
         encodingModule.encode(reader, writer);
 
         reader = new MyNetworkReader("http", "myfirstappwith.appspot.com", "/index.html");
